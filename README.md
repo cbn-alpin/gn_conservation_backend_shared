0a5fa76cf890fd261a74655a3ce4d26148a31efb
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2022-08-03 12:17:10 +0200

    Feat: first release

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# gn_conservation_backend_shared
-Code du backend partagé entre les modules Conservation de GeoNature.
+# Backend partagé des modules Conservation
+
+Code du backend partagé entre les modules Conservation de GeoNature (SHT, SFT, SHS, priority_flora).
+
+Pour l'instant, cela concerne principalement des éléments partagés de la base de données, utilisés dans les révisions Alembic. 
---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+from pathlib import Path
+
+
+root_dir = Path(__file__).absolute().parent
+with (root_dir / "VERSION").open() as f:
+    version = f.read()
+with (root_dir / "README.md").open() as f:
+    long_description = f.read()
+with (root_dir / "requirements.in").open() as f:
+    requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="gn_conservation_backend_shared",
+    version=version,
+    description="Python lib for shared backend files between conservation modules.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer="CBNA Development Team",
+    maintainer_email="adminsys@cbn-alpin.fr",
+    url="https://github.com/cbn-alpin/gn_conservation_backend_shared",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    package_data={
+        "gn_conservation_backend_shared.migrations": ["data/*.sql", "data/*.csv"]
+    },
+    install_requires=requirements,
+    entry_points={
+        "alembic": [
+            "migrations = gn_conservation_backend_shared.migrations:versions",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
