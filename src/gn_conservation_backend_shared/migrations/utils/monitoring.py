9acfa887c7c0f6808eab60737f14bef9ff402a5b
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2024-05-07 16:30:18 +0200

    fix(migrations/utils): make 'delete_sites' generic

--- src/gn_conservation_backend_shared/migrations/utils/monitoring.py
+++ src/gn_conservation_backend_shared/migrations/utils/monitoring.py
@@ -5,6 +5,24 @@ import sqlalchemy.sql as text
 def delete_sites(module_code):
     operation = text(
         """
+        -- Create temporary table to keep in memory all the sites linked to module
+        CREATE TEMPORARY TABLE sites_module(
+        gid serial4 NOT NULL,
+        id_base_site int4 NOT NULL,
+        id_module int4 NOT NULL
+        );
+
+        -- Insert all sites linked to module
+        INSERT INTO sites_module(id_base_site, id_module)
+        SELECT 
+            csm.id_base_site,
+            csm.id_module
+        FROM gn_monitoring.cor_site_module csm
+        JOIN gn_commons.t_modules tm
+            ON tm.id_module = csm.id_module
+        WHERE tm.module_code = :moduleCode
+        ;
+
         -- Unlink sites from module
         DELETE FROM gn_monitoring.cor_site_module
             WHERE id_module = (
@@ -15,8 +33,11 @@ def delete_sites(module_code):
 
         -- Delete sites
         DELETE from gn_monitoring.t_base_sites WHERE id_base_site IN (
-            SELECT id_base_site
-            FROM pr_monitoring_habitat_station.t_transects
+            SELECT sm.id_base_site
+            FROM gn_monitoring.cor_site_module  csm
+            LEFT JOIN sites_module sm
+                ON csm.id_base_site = sm.id_base_site
+            WHERE csm.id_base_site = sm.id_base_site
         ) ;
         """
     )
